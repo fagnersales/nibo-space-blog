ed6bfce2e1eb6cf0a1f94d5a03836723be813d57
Fagner Sales <fs.fagnersales@gmail.com>
2023-05-19 16:24:44 -0300

    ratelimiter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 
-import { RouterOutputs, api } from "~/utils/api";
+import { type RouterOutputs, api } from "~/utils/api";
 
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
